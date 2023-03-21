7831b0a53a8a0ac7a794b5ec5c71b386c396e44e
Naren <naren@alpastechnology.com>
2023-03-21 22:07:30 +0545

    Updated Package

---
+++ pm2.stage.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+      {
+        name: 'Slush Dating App (PROD)',
+        exec_mode: 'cluster',
+        instances: 'max', // Or a number of instances
+        script: './dist/main.js',
+        args: 'start',
+        env_local: {
+          APP_ENV: 'local' // APP_ENV=local
+        },
+        env_development: {
+          PORT: 3001,
+          APP_ENV: 'dev' // APP_ENV=dev
+        },
+        env_production: {
+          PORT: 3001,
+          APP_ENV: 'prod' // APP_ENV=prod
+        }
+      }
+    ]
+  }
