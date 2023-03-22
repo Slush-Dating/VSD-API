4a54d369b69984750450ede9057841c3b4806687
Naren <naren@alpastechnology.com>
2023-03-22 13:33:37 +0545

    Added isReported false condition for my matches

--- src/fixtures/fixtures.service.ts
+++ src/fixtures/fixtures.service.ts
@@ -345,6 +345,7 @@ export class FixturesService {
         .innerJoin(Participant, 'p1', 'p1.id = f.first_participant_id')
         .innerJoin(Participant, 'p2', 'p2.id = f.second_participant_id')
         .where('f.status = :liked', { liked: FixtureStatus.LIKED })
+        .where('f.is_reported = :false')
         .andWhere('p2.user_id = :authUserId', { authUserId })
         .having('mutual_like > 0');
 
