b964c2dd739ce8b4723dde7863c80dc11219ae61
Naren <naren@alpastechnology.com>
2023-03-22 14:31:29 +0545

    liked report user

--- src/fixtures/fixtures.service.ts
+++ src/fixtures/fixtures.service.ts
@@ -345,7 +345,8 @@ export class FixturesService {
         }, 'mutual_like')
         .innerJoin(Participant, 'p1', 'p1.id = f.first_participant_id')
         .innerJoin(Participant, 'p2', 'p2.id = f.second_participant_id')
-        .where('f.status = :liked', { liked: FixtureStatus.LIKED })        
+        .where('f.status = :liked', { liked: FixtureStatus.LIKED })   
+        .andWhere('f.is_reported = :is_reported', {is_reported: false})     
         .andWhere('p2.user_id = :authUserId', { authUserId })
         .having('mutual_like > 0');
 
