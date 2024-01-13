df2e3328506c58a3c29a712f7b4b6b051729d756
Naren <naren.mhrj@gmail.com>
2024-01-13 18:36:47 +0545

    CRON will check for events every 30 seconds

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -541,7 +541,7 @@ export class EventsService {
       return await this.eventRepo.query(
         `SELECT e.*
         FROM events e
-        WHERE (CURRENT_TIMESTAMP + INTERVAL 15 MINUTE) > e.starts_at
+        WHERE (CURRENT_TIMESTAMP + INTERVAL 30 SECOND) > e.starts_at
         AND e.status = ?`,
         [EventStatusEnum.NOT_YET_STARTED],
       );
