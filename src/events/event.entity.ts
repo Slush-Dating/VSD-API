47002003dbf39fe8cf9ceb511921a7ee9687fbdf
HetarthiPandit <hetarthipandit2002@gmail.com>
2024-01-25 16:23:07 +0530

    add password field in event module and check validation in book event

--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -87,6 +87,19 @@ export class Event extends BaseEntity {
   @Column({ type: 'int' })
   minAge: number;
 
+
+  /**
+ * @example Passsword
+ */
+  @MinLength(3)
+  @IsString()
+  @IsNotEmpty()
+  @Expose()
+  @Column()
+  password: string;
+
+
+
   /**
    * @example 21
    */
