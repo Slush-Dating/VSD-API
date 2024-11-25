85c7044706b022f522e4862d96e6610348b3f6e5
Mohit4248 <mohitthakkar4248@gmail.com>
2024-11-25 12:42:49 +0530

    Fixed event notification issue

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -680,7 +680,7 @@ export class EventsService {
             FROM events e
             WHERE e.status != ?
             AND e.notify_before_one IS NULL
-            AND TIMESTAMPDIFF(SECOND, CURRENT_TIMESTAMP, e.starts_at) < 60
+            AND TIMESTAMPDIFF(MINUTE, CURRENT_TIMESTAMP, e.starts_at) = 1
             AND e.ends_at > CURRENT_TIMESTAMP`,
         [EventStatusEnum.CANCELLED],
       );
